f8c8f014defa2e6e18eae3953b17f8d3c4d441f4
TLExpress <65728108+TLExpress@users.noreply.github.com>
2021-10-09 20:04:02 +0800

    scsHash, scsFileAnalyzer added

---
+++ SII_Decrypt.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#ifndef SII_DECRYPT_HPP
+#define SII_DECRYPT_HPP
+#include <cstdint>
+
+#ifdef SII_DECRYPT
+#define SII_DECRYPT_DLL __declspec(dllexport)
+#else
+#define SII_DECRYPT_DLL __declspec(dllimport)
+#endif
+
+typedef uint32_t bool32_t;
+
+extern "C" SII_DECRYPT_DLL uint32_t	__stdcall APIVersion();
+extern "C" SII_DECRYPT_DLL int32_t	__stdcall GetMemoryFormat(char* Mem,size_t Size);
+extern "C" SII_DECRYPT_DLL int32_t	__stdcall GetFileFormat(char* FileName);
+extern "C" SII_DECRYPT_DLL bool32_t	__stdcall IsEncryptedMemory(char* Mem, size_t Size);
+extern "C" SII_DECRYPT_DLL bool32_t	__stdcall IsEncryptedFile(char* FileName);
+extern "C" SII_DECRYPT_DLL bool32_t	__stdcall IsEncodedMemory(char* Mem, size_t Size);
+extern "C" SII_DECRYPT_DLL bool32_t	__stdcall IsEncodedFile(char* FileName);
+extern "C" SII_DECRYPT_DLL bool32_t	__stdcall Is3nKEncodedMemory(char* Mem, size_t Size);
+extern "C" SII_DECRYPT_DLL bool32_t	__stdcall Is3nKEncodedFile(char* FileName);
+extern "C" SII_DECRYPT_DLL int32_t	__stdcall DecryptMemory(char* Input, size_t InSize, char* output, size_t* OutSize);
+extern "C" SII_DECRYPT_DLL int32_t	__stdcall DecryptFile(char* InputFile, char* OutputFile);
+extern "C" SII_DECRYPT_DLL int32_t	__stdcall DecryptFileInMemory(char* InputFile, char* OutputFile);
+extern "C" SII_DECRYPT_DLL int32_t	__stdcall DecodeMemoryHelper(char* Input, size_t InSize, char* output, size_t * OutSize, char** Helper);
+extern "C" SII_DECRYPT_DLL int32_t	__stdcall DecodeMemory(char* Input, size_t InSize, char* output, size_t * OutSize);
+extern "C" SII_DECRYPT_DLL int32_t	__stdcall DecodeFile(char* InputFile, char* OutputFile);
+extern "C" SII_DECRYPT_DLL int32_t	__stdcall DecodeFileInMemory(char* InputFile, char* OutputFile);
+extern "C" SII_DECRYPT_DLL int32_t	__stdcall DecryptAndDecodeMemoryHelper(char* Input, size_t InSize, char* output, size_t * OutSize, char** Helper);
+extern "C" SII_DECRYPT_DLL int32_t	__stdcall DecryptAndDecodeMemory(char* Input, size_t InSize, char* output, size_t * OutSize);
+extern "C" SII_DECRYPT_DLL int32_t	__stdcall DecryptAndDecodeFile(char* InputFile, char* OutputFile);
+extern "C" SII_DECRYPT_DLL int32_t	__stdcall DecryptAndDecodeFileInMemory(char* InputFile, char* OutputFile);
+extern "C" SII_DECRYPT_DLL int32_t	__stdcall FreeHelper(char** Helper);
+
+#endif
--- hashTester.cpp
+++ hashTester.cpp
@@ -1,19 +1,54 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <cstdlib> 
 #include "scshash.hpp"
-#include "3nK.hpp"
+#include "scsFileAnalyzer.hpp"
+#include "SII_Decrypt.hpp"
+#include "zlib.h"
 
 int main(int argc, char** argv)
 {
-	
-	//while (true)
-	{
-		std::string testString(*(argv+1));
-		//std::cin >> testString;
-		//std::string result = testString + "\b\b\b\b_3nK.sii";
+	/*size_t size, b, bsize;
+	std::ifstream istream(*(argv + 1), std::ios::in | std::ios::binary);
+	istream.seekg(0, std::ios::end);
+	size = istream.tellg();
+	istream.seekg(0, std::ios::beg);
+	char* ibuff = new char[size], * obuff = new char[b=bsize = compressBound(size)];
+	istream.read(ibuff, size);
+	istream.close();
+	compress3((Bytef*)obuff, (uLongf*)&bsize, (Bytef*)ibuff, size,15);
+	delete[] ibuff;
+	std::ofstream ostream(*(argv + 1), std::ios::out | std::ios::binary);
+	ostream.write(obuff, bsize);
+	ostream.close();
+	delete[] obuff;
+	std::cout << "compressed" << std::endl;
+	system("pause");
+	istream.open(*(argv + 1), std::ios::in | std::ios::binary);
+	ibuff = new char[b];
+	istream.read(ibuff, bsize);
+	istream.close();
+	obuff = new char[size];
+	uncompress3((Bytef*)obuff, (uLongf*)&b, (Bytef*)ibuff, size,15);
+	delete[] ibuff;
+	ostream.open(*(argv + 1), std::ios::out | std::ios::binary);
+	ostream.write(obuff, size);
+	ostream.close();
+	delete[] obuff;
+	std::cout << "uncompressed" << std::endl;
+	system("pause");*/
 
-		S3nKTranscoder* transcoder = new S3nKTranscoder();
-		transcoder->transcodeFileInMemory(testString, testString);
-	}
+	/*
+		std::string a;
+		std::cin >> a;
+		if (a._Equal("0"))break;
+		std::cout << std::hex << getHashFromString(a) << std::endl;
+	}*/
+	std::string str;
+	std::cin >> str;
+	std::list<std::string>* list = extractFileToList(str, scsFileAccessMethod::inMemory);
+	for (auto const& it : *list)
+		std::cout << it << std::endl;
 	return 0;
 }
